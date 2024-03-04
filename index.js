e486893a042efa65f13921f673373d970cf6c62c
raffny <162116479+raffny@users.noreply.github.com>
2024-03-04 17:43:56 +0000

    Added helper function

---
+++ index.js
@@ -0,0 +1,10 @@
+// Define a helper function to calculate the square of a number
+function square(number) {
+    return number * number;
+  }
+  
+  // Main code
+  var num = 5;
+  var result = square(num);
+  console.log("The square of", num, "is", result);
+  
